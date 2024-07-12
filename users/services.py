1d9d6bc553701517def3988e721ff278d1767d80
mariaonderoz <mskorneeva.87@gmail.com>
2024-07-12 12:06:10 +0700

    update

---
+++ users/services.py
@@ -0,0 +1,23 @@
+import stripe
+from config.settings import STRIPE_API_KEY
+
+stripe.api_key = STRIPE_API_KEY
+
+def create_stripe_price(amount):
+    return stripe.Price.create(
+        currency="usd",
+        unit_amount=amount * 100,
+        recurring={"interval": "month"},
+        product_data={"name": "Payment"},
+    )
+
+def create_stripe_session(price):
+    session = stripe.checkout.Session.create(
+        success_url="http://127.0.0.1:8000/",
+        line_items=[{"price": price.get("id"), "quantity": 1}],
+        mode="payment",
+    )
+    return session.get("id"), session.get("url")
+
+
+
